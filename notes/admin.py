f26141be5748c4680b9a8e4818717d93d0673004
Diego Leal <diego.leal@shipwell.com>
2023-11-22 18:41:54 -0300

    feat: creating models; Adding models to admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from notes.models import Trade
+from notes.models import Asset
+from notes.models import Trigger
+
+
+admin.site.register(Trade)
+admin.site.register(Asset)
+admin.site.register(Trigger)
